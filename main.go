203e3cb23a2ccd392dc52f7801925aca64b92257
codemarquis <geraldezeani@pm.me>
2025-08-07 12:24:02 +0200

    Fix gosec issues and update CI workflow

--- main.go
+++ main.go
@@ -91,6 +91,7 @@ func main() {
 	srv := &http.Server{
 		Addr:    ":" + port,
 		Handler: router,
+		ReadHeaderTimeout: 5 * time.second, // prevents Slowloris attacks
 	}
 
 	log.Printf("Serving on port: %s\n", port)
