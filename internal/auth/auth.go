ca8f88b8d3ef1220913e113cca7f90ffc49c399e
codemarquis <geraldezeani@pm.me>
2025-08-07 06:22:01 +0200

    Break GETAPI key for CI test

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -8,16 +8,6 @@ import (
 
 var ErrNoAuthHeaderIncluded = errors.New("no authorization header included")
 
-// GetAPIKey -
 func GetAPIKey(headers http.Header) (string, error) {
-	authHeader := headers.Get("Authorization")
-	if authHeader == "" {
-		return "", ErrNoAuthHeaderIncluded
-	}
-	splitAuth := strings.Split(authHeader, " ")
-	if len(splitAuth) < 2 || splitAuth[0] != "ApiKey" {
-		return "", errors.New("malformed authorization header")
-	}
-
-	return splitAuth[1], nil
+    return "", errors.New("always broken for CI test")
 }
